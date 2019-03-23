f5511dffa647d10be20cdf9b3af9b93356102be8
Scott Atkins <scott@kins.dev>
2019-03-23 16:50:05 -0700

    Fix for missing data values

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! [ -f /tmp/data.csv ] ; then
-    echo "Time,Battery,Meat Temp,Smoke Temp" > /tmp/data.csv
+    echo "Time,Battery,Smoke Temp,Meat Temp" > /tmp/data.csv
     gpio mode 15 out
     gpio write 15 1
     gpio mode 4 out
