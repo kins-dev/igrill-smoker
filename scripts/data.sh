105bfa3401edc09497c06686a22dadba20e5540e
Scott Atkins <scott@kins.dev>
2019-05-10 11:25:32 -0700

    Split out kasa functions

--- scripts/data.sh
+++ scripts/data.sh
@@ -26,6 +26,10 @@ source "${IGRILL_UTL_DIR}/leds.sh"
 # shellcheck source=utils/sounds.sh
 source "${IGRILL_UTL_DIR}/sounds.sh"
 
+# pull in sound functions
+# shellcheck source=utils/kasa.sh
+source "${IGRILL_UTL_DIR}/kasa.sh"
+
 # Functions
 
 # Used with trap to make sure the file is written before the script exits
@@ -47,41 +51,7 @@ function LoadConfig () {
 }
 
 # Should be moved out to another file
-function SetKasaState() {
-    local STATE
-    local MSG
-    if [ "$#" -eq "1" ]; then
-        MSG="Turning hotplate $1"
-    elif [ "$#" -eq "2" ]; then
-        MSG="Turning hotplate $1 due to $2"
-    else
-        echo "Wrong number of arguements to SetKasaState"
-        echo "Expected 1 or 2, got $#"
-        exit 1
-    fi
-    STATE="$1"
-    tplink-smarthome-api send "$TP_LINK_IP" '{"count_down":{"delete_all_rules":{}}}}'
-    # NOTE: api commands must be blocking as they take a second or two
-    # and another state update may come in
-    case "$STATE" in
-        "on")
-            KASA_STATE="lightgreen"
-            tplink-smarthome-api setPowerState "$TP_LINK_IP" true
-            # Force off after 5 minutes if there's no commands
-            tplink-smarthome-api send "$TP_LINK_IP" '{"count_down":{"add_rule":{"enable":1,"delay":300,"act":0,"name":"turn off"}}}'
-        ;;
-        "off")
-            KASA_STATE="red"
-            tplink-smarthome-api setPowerState "$TP_LINK_IP" false
-        ;;
-        *)
-            echo "bad value for hotplate state sent to SetKasaState"
-            echo "expected \"on\" or \"off\", got \"$STATE\""
-            exit 1
-        ;;
-    esac
-    echo "$MSG"
-}
+
 
 # Updates the stages (at a transition point)
 function WriteStages()
---
+++ scripts/utils/kasa.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# shellcheck disable=2034
+true
+# shellcheck disable=2086
+set -$-ue${DEBUG+xv}
+
+function SetKasaState() {
+    local STATE
+    local MSG
+    if [ "$#" -eq "1" ]; then
+        MSG="Turning hotplate $1"
+    elif [ "$#" -eq "2" ]; then
+        MSG="Turning hotplate $1 due to $2"
+    else
+        echo "Wrong number of arguements to SetKasaState"
+        echo "Expected 1 or 2, got $#"
+        exit 1
+    fi
+    STATE="$1"
+    tplink-smarthome-api send "$TP_LINK_IP" '{"count_down":{"delete_all_rules":{}}}}'
+    # NOTE: api commands must be blocking as they take a second or two
+    # and another state update may come in
+    case "$STATE" in
+        "on")
+            KASA_STATE="lightgreen"
+            tplink-smarthome-api setPowerState "$TP_LINK_IP" true
+            # Force off after 5 minutes if there's no commands
+            tplink-smarthome-api send "$TP_LINK_IP" '{"count_down":{"add_rule":{"enable":1,"delay":300,"act":0,"name":"turn off"}}}'
+        ;;
+        "off")
+            KASA_STATE="red"
+            tplink-smarthome-api setPowerState "$TP_LINK_IP" false
+        ;;
+        *)
+            echo "bad value for hotplate state sent to SetKasaState"
+            echo "expected \"on\" or \"off\", got \"$STATE\""
+            exit 1
+        ;;
+    esac
+    echo "$MSG"
+}
