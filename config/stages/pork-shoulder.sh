b5bb70ff35cd28fbeaf7accf7ff19838f41ebaf0
Scott Atkins <scott@kins.dev>
2019-06-15 09:43:20 -0700

    Making longer smoking session

--- config/stages/pork-shoulder.sh
+++ config/stages/pork-shoulder.sh
@@ -19,25 +19,32 @@ case "$STAGE" in
     1)
         # Warmup stage, keep plate at a cooler temp and limit temp rise
         STAGE_NAME="Warmup"
-        SMOKE_MID=180
+        SMOKE_MID=120
         MAX_TEMP_CHANGE=1
-        INTERNAL_TEMP=70
+        INTERNAL_TEMP=65
     ;;
     2)
+        # Smoke stage, keep plate at cooler temp, but allow bigger temp rise
+        STAGE_NAME="Smoke"
+        SMOKE_MID=160
+        MAX_TEMP_CHANGE=2
+        INTERNAL_TEMP=100
+    ;;
+    3)
         # Smoke stage, keep plate at cooler temp, but allow bigger temp rise
         STAGE_NAME="Smoke"
         SMOKE_MID=180
         MAX_TEMP_CHANGE=2
         INTERNAL_TEMP=120
     ;;
-    3)
+    4)
         # Cook stage, move hotplate to higher temp, allow bigger temp rise
         STAGE_NAME="Cook"
         SMOKE_MID=225
         MAX_TEMP_CHANGE=2
         INTERNAL_TEMP=190
     ;;
-    4|5)
+    5|6)
         # Keep warm stage, move hotplate to higher temp, allow bigger temp rise
         STAGE_NAME="Keep warm"
         SMOKE_MID=160
