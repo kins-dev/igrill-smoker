260c69c9932ed6501d5d2e0a40088e6d46c8ff33
Scott Atkins <scott@kins.dev>
2019-07-06 14:01:38 -0700

    updated pins

--- scripts/py_utils/constant.py
+++ scripts/py_utils/constant.py
@@ -25,13 +25,13 @@ SSR_CONTROL_BOARD_DETECT_REV                = "Auto"
 SSR_CONTROL_BOARD_PINS                      = {
     "LED":{
         "red":{
-            SSR_CONTROL_BOARD_REV_ss:23,
+            SSR_CONTROL_BOARD_REV_ss:22,
             SSR_CONTROL_BOARD_REV_sA:23,
             SSR_CONTROL_BOARD_REV_sB:2,
             SSR_CONTROL_BOARD_REV_sC:2
         },
         "green":{
-            SSR_CONTROL_BOARD_REV_ss:22,
+            SSR_CONTROL_BOARD_REV_ss:23,
             SSR_CONTROL_BOARD_REV_sA:22,
             SSR_CONTROL_BOARD_REV_sB:3,
             SSR_CONTROL_BOARD_REV_sC:3
