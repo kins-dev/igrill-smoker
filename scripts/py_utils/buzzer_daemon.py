2b272da5101ed93814e11886fb92041965c85073
Scott Atkins <scott@kins.dev>
2019-07-21 09:03:44 -0700

    Buzzer changes

--- scripts/py_utils/buzzer_daemon.py
+++ scripts/py_utils/buzzer_daemon.py
@@ -2,13 +2,16 @@
 #                       (https://git.kins.dev/igrill-smoker)
 # License:              MIT License
 #                       See the LICENSE file
+import pigpio
 import time
 import logging
 import argparse
 import configparser
+import threading
 import sys
 from Pyro5.api import expose, behavior, Daemon
 import constant
+import board
 from struct import pack
 from local_logging import SetupLog
 
@@ -23,6 +26,7 @@ class Buzzer(object):
         config.read(sys.path[0]+'/../../config/iGrill_config.ini')
         loglevel = config.get("Logging", "LogLevel", fallback="Error")
         logfile = config.get("Logging", "LogFile", fallback="")
+        boardVal = config.get("SSR", "Board")
 
         parser = argparse.ArgumentParser(
             description='Runs a thread to control the buzzer')
@@ -43,12 +47,49 @@ class Buzzer(object):
         options = parser.parse_args()
 
         SetupLog(options.log_level, options.log_destination)
-
+        boardVal = board.DetectBoard(boardVal)
+        if (constant.SSR_CONTROL_BOARD_DISABLED == boardVal):
+            sys.exit(1)
+        self.m_boardVal = boardVal
         self.m_exitCode = 0
-        self.StartThread()
+        self.m_active = True
+        self.m_lock = threading.Lock()
+        self.m_thread = threading.Thread(target=self.StartThread, args=())
+        self.m_thread.start()
 
     def StartThread(self):
         logging.debug("Starting thread")
+        pi = pigpio.pi()
+        item = constant.SSR_CONTROL_BOARD_ITEMS["Buzzer"][self.m_boardVal]
+        offVal = 1000000
+        if item[constant.SSR_CONTROL_BOARD_ITEM_VALUE] == constant.SSR_CONTROL_BOARD_VALUES_STANDARD:
+            offVal = 0
+        pin = item[constant.SSR_CONTROL_BOARD_ITEM_IO]
+        while True:
+            with self.m_lock:
+                lowBattery = self.m_lowBattery
+                done = self.m_done
+                active = self.m_active
+            if not active:
+                pi.hardware_PWM(pin,2000,offVal)
+                break
+            if lowBattery:
+                logging.debug("Low battery")
+                pi.hardware_PWM(pin,2000,500000)
+                time.sleep(0.5)
+                pi.hardware_PWM(pin,3000,500000)
+                time.sleep(0.5)
+            elif done:
+                logging.debug("Done")
+                pi.hardware_PWM(pin,2500,500000)
+                time.sleep(0.5)
+                pi.hardware_PWM(pin,2000,offVal)
+                time.sleep(0.5)
+            else:
+                logging.debug("Quiet")
+                pi.hardware_PWM(pin,2000,offVal)
+                time.sleep(0.5)
+                
         # run the thread
 
     def Done(self):
@@ -70,6 +111,8 @@ class Buzzer(object):
         return self.m_exitCode
 
     def Exit(self):
+        self.m_active = False
+        self.m_thread.join()
         logging.debug("Closing socket")
         self.m_daemon.shutdown()
 
