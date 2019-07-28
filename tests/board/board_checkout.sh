6ce7baa128b23f565c162f842059d73e6397a2b5
Scott Atkins <scott@kins.dev>
2019-07-28 11:45:00 -0700

    adding buzzer tests

--- tests/board/board_checkout.sh
+++ tests/board/board_checkout.sh
@@ -7,29 +7,58 @@ true
 # shellcheck disable=2086
 set -$-ue${DEBUG+xv}
 
-pushd ../../scripts
+
+VALUE=${IGRILL_BAS_DIR:-}
+if [ -z "${VALUE}" ]; then
+    # https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    IGRILL_BAS_DIR="$(readlink -f "${DIR}/../..")"
+    export IGRILL_BAS_DIR
+fi
+
+# shellcheck source=scripts/utils/paths.sh
+source "${IGRILL_BAS_DIR}/scripts/utils/paths.sh"
+
 echo "Check the smoking complete light is on and hit enter"
-python3 -m pygrill.board.leds --done
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds --done
 read
 echo "Check the low battery light is on and hit enter"
-python3 -m pygrill.board.leds --low_battery
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds --low_battery
 read
 echo "Check the cold light is (if supported) on and hit enter"
-python3 -m pygrill.board.leds --cold
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds --cold
 read
 echo "Check the cool light is (if supported) on and hit enter"
-python3 -m pygrill.board.leds --cool
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds --cool
 read
 echo "Check the perfect light (if supported) is on and hit enter"
-python3 -m pygrill.board.leds --perfect
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds --perfect
 read
 echo "Check the warm light is (if supported) on and hit enter"
-python3 -m pygrill.board.leds --warm
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds --warm
 read
 echo "Check the hot light is (if supported) on and hit enter"
-python3 -m pygrill.board.leds --hot
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds --hot
 read
 echo "Check the lights are off and hit enter"
-python3 -m pygrill.board.leds
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.leds
 read
-popd
+echo "Starting buzzer daemon"
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.buzzer_daemon --log-level Error & disown
+echo "Check buzzer turns on and off and hit enter"
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.buzzer_client --done
+read
+echo "Check buzzer alternates betwen two tones and hit enter"
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.buzzer_client --low_battery
+read
+echo "Check buzzer is off hit enter"
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.buzzer_client
+read
+echo "Shutting down buzzer daemon"
+PYTHONPATH="${IGRILL_SCR_DIR}" python3 -m pygrill.board.buzzer_client --exit
