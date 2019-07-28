b1153287f9832cbe28c12e553896a2c6f6d0653b
Scott Atkins <scott@kins.dev>
2019-07-28 11:12:14 -0700

    Full LED checkout

--- tests/board/board_checkout.sh
+++ tests/board/board_checkout.sh
@@ -8,7 +8,28 @@ true
 set -$-ue${DEBUG+xv}
 
 pushd ../../scripts
-echo "Check the green light is on and hit enter"
+echo "Check the smoking complete light is on and hit enter"
 python3 -m pygrill.board.leds --done
+read
+echo "Check the low battery light is on and hit enter"
+python3 -m pygrill.board.leds --low_battery
+read
+echo "Check the cold light is (if supported) on and hit enter"
+python3 -m pygrill.board.leds --cold
+read
+echo "Check the cool light is (if supported) on and hit enter"
+python3 -m pygrill.board.leds --cool
+read
+echo "Check the perfect light (if supported) is on and hit enter"
+python3 -m pygrill.board.leds --perfect
+read
+echo "Check the warm light is (if supported) on and hit enter"
+python3 -m pygrill.board.leds --warm
+read
+echo "Check the hot light is (if supported) on and hit enter"
+python3 -m pygrill.board.leds --hot
+read
+echo "Check the lights are off and hit enter"
+python3 -m pygrill.board.leds
 read
 popd
